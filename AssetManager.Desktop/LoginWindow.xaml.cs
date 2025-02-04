a36a1f070b2b1d1c141386c70e679a926dc4eb34
TylerGoodall <tylergoodall@hotmail.co.uk>
2025-02-04 12:59:47 +0000

    a

--- AssetManager.Desktop/LoginWindow.xaml.cs
+++ AssetManager.Desktop/LoginWindow.xaml.cs
@@ -1,12 +1,17 @@
+using System.Diagnostics;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Windows;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace AssetManager.Desktop;
 
 public partial class LoginWindow : Window
 {
-    private string loginURL = "https://developer.api.autodesk.com/authentication/v2/authorize";
+    //private string loginURL = "https://developer.api.autodesk.com/authentication/v2/authorize";
     private string tokenURL = "https://developer.api.autodesk.com/authentication/v2/token";
     string clientID = "";
     private string redirect = "";
@@ -18,39 +23,59 @@ public partial class LoginWindow : Window
 
         if (Environment.GetCommandLineArgs().Length > 1 && Environment.GetCommandLineArgs()[1].StartsWith(redirect))
         {
-            
+            Uri uri = new Uri(Environment.GetCommandLineArgs()[1]);
+            string authCode = HttpUtility.ParseQueryString(uri.Query).Get("code");
+            GetAccessToken(authCode);
         }
     }
 
     private async void LoginButton_Click(object sender, RoutedEventArgs e)
     {
-        using (HttpClient client = new HttpClient())
+        try
         {
-            string nonce = "";
-            Pkce pkce = new Pkce();
-            var loginData = new Dictionary<string, string>
-            {
-                { "response_type", "code" },
-                { "client_id", clientID },
-                { "redirect_uri", redirect },
-                { "scope", "data:read" },
-                { "nonce", nonce },
-                { "prompt", "login" },
-                { "code_challenge", pkce.CodeChallenge },
-                { "code_challenge_method", "S256" }
-            };
+            string nonce = GenerateNonce();
+            Pkce pkce = GeneratePkce();
+            string loginURL = $"https://developer.api.autodesk.com/authentication/v2/authorize?response_type=code&client_id={clientID}&redirect_uri={redirect}&scope=data:read%20user-profile:read&nonce={nonce}&prompt=login&code_challenge={pkce.CodeChallenge}&code_challenge_method=S256";
 
-            var loginParameters = new FormUrlEncodedContent(loginData);
-            var loginRepsonse = await client.PostAsync(loginURL, loginParameters);
-            string loginResponseData = await loginRepsonse.Content.ReadAsStringAsync();
-            Uri uri = new Uri(loginResponseData);
-            string authCode = HttpUtility.ParseQueryString(uri.Query).Get("code");
+            Process.Start(new ProcessStartInfo{FileName = loginURL, UseShellExecute = true});
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error Logging in: {ex.Message}");
+            throw;
         }
     }
 
-    private async void GetAcessToken(string authCode)
+    private async void GetAccessToken(string authCode)
     {
-        
+        using (HttpClient client = new HttpClient())
+        {
+            var tokenContent = new Dictionary<string, string>
+            {
+                { "grant_type", grantType },
+                { "client_id", clientID },
+                { "code_verifier", "" },
+                { "code", authCode },
+                { "redirect_uri", redirect }
+            };
+            
+            var tokenParameters = new FormUrlEncodedContent(tokenContent);
+            var tokenResponse = await client.PostAsync(tokenURL, tokenParameters);
+            var tokenResponseContent = await tokenResponse.Content.ReadAsStringAsync();
+            var tokenData = JsonConvert.DeserializeObject<TokenData>(tokenResponseContent);
+            MessageBox.Show($"Access Token: {tokenData.AccessToken}");
+        }
+    }
+
+    private async void GetUserData(string accessToken)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var userDataResponse = await client.GetAsync("https://api.userprofile.autodesk.com/userinfo");
+            string userDataContent = await userDataResponse.Content.ReadAsStringAsync();
+            MessageBox.Show($"User Data: {userDataContent}");
+        }
     }
 
     public class Pkce
@@ -66,5 +91,65 @@ public partial class LoginWindow : Window
         public string ExpiresIn { get; set; }
         public string RefreshToken { get; set; }
         
+    }
+
+    public string GenerateNonce()
+    {
+        return Guid.NewGuid().ToString("N");
+    }
+
+    public Pkce GeneratePkce()
+    {
+        var pkce = new Pkce
+        {
+            CodeVerifier = CryptoRandom.CreateUniqueId(32)
+        };
+        using (var sha256 = SHA256.Create())
+        {
+            // Here we create a hash of the code verifier
+            var challengeBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(pkce.CodeVerifier));
+
+            // and produce the "Code Challenge" from it by base64Url encoding it.
+            pkce.CodeChallenge = Base64Url.Encode(challengeBytes);
+        }
+        return pkce;
+    }
+    
+    public static class CryptoRandom
+    {
+        // This method generates a secure random string of a specified length
+        public static string CreateUniqueId(int length)
+        {
+            // Create a byte array to hold random data
+            byte[] randomBytes = new byte[length];
+
+            // Using RNGCryptoServiceProvider to generate cryptographically secure random data
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomBytes);
+            }
+
+            // Convert the random bytes into a Base64 string and make it URL-safe by removing padding and replacing characters
+            return Convert.ToBase64String(randomBytes)
+                .TrimEnd('=')
+                .Replace('+', '-')
+                .Replace('/', '_');
+        }
+    }
+
+    public static class Base64Url
+    {
+        public static string Encode(byte[] input)
+        {
+            string base64 = Convert.ToBase64String(input);
+
+            // Remove padding character '=' from the end
+            base64 = base64.TrimEnd('=');
+
+            // Replace '+' with '-' and '/' with '_'
+            base64 = base64.Replace('+', '-').Replace('/', '_');
+
+            return base64;
+        }
     }
 }
