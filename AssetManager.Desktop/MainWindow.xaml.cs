1ae1e9d864e704406bff2df08b0abda77f2c7d68
TylerGoodall <tylergoodall@hotmail.co.uk>
2025-02-15 13:21:35 +0000

    basic comments - doesn't use asset Id

--- AssetManager.Desktop/MainWindow.xaml.cs
+++ AssetManager.Desktop/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System.Windows;
+using AssetManager.Infrastructure.Data;
 using AssetManager.Infrastructure.Services;
+using AssetManager.Infrastructure.Models;
 using Autodesk.Authentication.Model;
 using Microsoft.Win32;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace AssetManager.Desktop
 {
@@ -67,14 +71,113 @@ namespace AssetManager.Desktop
             this.Close();
         }
 
-        private void BtnAddComment_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
         private void BtnViewComment_Click(object sender, RoutedEventArgs e)
         {
-            
+            try
+            {
+                BtnAddComment.Visibility = Visibility.Visible;
+                CommentContent.Visibility = Visibility.Visible;
+                ListComments.Visibility = Visibility.Visible;
+                ListAllComments();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+        
+        private async void BtnAddComment_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string comment = CommentContent.Text;
+
+                if (!string.IsNullOrEmpty(comment) && !comment.StartsWith("Add a comment"))
+                {
+                    MongoConnection database = new MongoConnection();
+                    Comment commentContent = new Comment
+                    {
+                        CommentId = ObjectId.GenerateNewId(),
+                        AssetId = "001",
+                        UserId = Environment.GetEnvironmentVariable("userId"),
+                        Content = comment,
+                        CreatedDateTime = DateTime.Now
+                    };
+                    
+                    await database.Comments.InsertOneAsync(commentContent);
+                    ListNewComment(commentContent);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        private async void ListAllComments()
+        {
+            try
+            {
+                List<Comment> comments = await GetAllComments();
+                List<CommentItem> commentItems = new List<CommentItem>();
+                
+                foreach (Comment comment in comments)
+                {
+                    commentItems.Add(new CommentItem {User = "test", Content = comment.Content, CreatedDateTime = comment.CreatedDateTime});
+                }
+
+                if (commentItems.Count != 0)
+                {
+                    foreach (CommentItem commentItem in commentItems)
+                    {
+                        ListComments.Items.Add(commentItem);
+                    }
+                }
+                else
+                {
+                    ListComments.Items.Add(new CommentItem
+                    {
+                        User = String.Empty, Content = "There are no Comments", CreatedDateTime = DateTime.MinValue
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Error: {e.Message}");
+            }
+        }
+
+        private async void ListNewComment(Comment commentItem)
+        {
+            List<Comment> comments = await GetAllComments();
+                
+            foreach (Comment comment in comments)
+            {
+                if (commentItem.CommentId == comment.CommentId)
+                    ListComments.Items.Add(new CommentItem {User = "test", Content = comment.Content, CreatedDateTime = comment.CreatedDateTime});
+            }
+        }
+        
+        private async Task<List<Comment>> GetAllComments(string assetId = "001")
+        {
+            try
+            {
+                MongoConnection database = new MongoConnection();
+                var allComments = await database.Comments.Find(x => x.AssetId == assetId ).ToListAsync();
+                return allComments;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Error: {e.Message}");
+                throw;
+            }
+        }
+
+        private class CommentItem
+        {
+            public string User { get; set; }
+            public string Content { get; set; }
+            public DateTime CreatedDateTime { get; set; }
         }
     }
 }
--- AssetManager.Infrastructure/Models/Comment.cs
+++ AssetManager.Infrastructure/Models/Comment.cs
@@ -6,11 +6,11 @@ namespace AssetManager.Infrastructure.Models;
 
 public class Comment
 {
-    [BsonElement("_commentId")]
+    [BsonElement("_id")]
     public ObjectId CommentId { get; set; }
     
     [BsonElement("_fileId")]
-    public ObjectId AssetId { get; set; }
+    public string AssetId { get; set; }
     
     [BsonElement("_userId")]
     public string UserId { get; set; }
