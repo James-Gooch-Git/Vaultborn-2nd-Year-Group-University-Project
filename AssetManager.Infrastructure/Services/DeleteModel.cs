c54a3aee893a835fa20d0aa09bbca60baeffa3e3
TomGrout <161628328+TomGrout@users.noreply.github.com>
2025-02-20 16:46:05 +0000

    delete

---
+++ AssetManager.Infrastructure/Services/DeleteModel.cs
@@ -0,0 +1,13 @@
+using AssetManager.Infrastructure.Data;
+
+namespace AssetManager.Infrastructure.Services;
+
+public class DeleteModel
+{
+    MongoConnection database = new MongoConnection();
+
+    public static void DeleteSpecificModel(string itemId)
+    {
+        
+    }
+}
