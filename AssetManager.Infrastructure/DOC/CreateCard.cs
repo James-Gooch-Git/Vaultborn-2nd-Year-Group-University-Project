328129b735abfa578d20d1d44755b9a1007d2720
TomGrout <161628328+TomGrout@users.noreply.github.com>
2025-03-27 11:15:37 +0000

    aws secrets

--- AssetManager.Infrastructure/DOC/CreateCard.cs
+++ AssetManager.Infrastructure/DOC/CreateCard.cs
@@ -29,7 +29,7 @@ public class CreateCard
                     { "model_id", modelId },
                     { "user_id", userId },
                     { "creation_date", DateTime.Now },
-                   { "deck_id", string.IsNullOrEmpty(deckId) ? BsonNull.Value : deckId }
+                   { "deck_id", deckId }
                 };
 
             // Store the image as binary data, not as a base64 string
